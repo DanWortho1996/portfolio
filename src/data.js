436ff9215043156136025afde92814593b11d27d
Daniel Worthington <daniel.worthington96@outlook.com>
2024-11-18 20:32:50 +0000

    chore: searchbar content fill

--- src/data.js
+++ src/data.js
@@ -1,6 +1,6 @@
 export const data = [
-    { title: 'Home', content: 'Content for option 1.', link: '/' },
-    { title: 'Coding', content: 'Content for option 2.', link: 'code' },
-    { title: 'Profile Page', content: 'Content for option 3.', link: '/profilepage' },
+    { title: 'Home', content: 'Content for home', link: '/' },
+    { title: 'Coding', content: 'Content for software development', link: '/code' },
+    { title: 'Profile Page', content: 'Content for profile page', link: '/profilepage' },
 ];
   
