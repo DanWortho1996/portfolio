4bc73260a61cb3a5bcbce0f65f229e0d02790f17
Daniel Worthington <daniel.worthington96@outlook.com>
2025-02-26 15:38:29 +0000

    bug-fix: spell check

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/Header/Header';
+import Header from './components/header/Header';
 import LandingPage from './pages/landingPage/LandingPage';
 import CodePage from './pages/codePage/CodePage';
 import ProfilePage from './pages/profilePage/ProfilePage';
 import { data } from './data';
-import Footer from './components/Footer/Footer';
+import Footer from './components/footer/Footer';
 
 const App = () => {
   return (
